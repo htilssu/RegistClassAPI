4a2abf3c99a2fd5ff43c99cb6bdbe13e4b7ca689
htilssu <tolashuu@gmail.com>
2024-02-28 04:36:47 +0700

    add lhp log on discord

--- app/services/DiscordBot.ts
+++ app/services/DiscordBot.ts
@@ -1,11 +1,47 @@
-import {Client} from "discord.js";
+import {Channel, Client, EmbedBuilder, HexColorString} from "discord.js";
 
 const client: Client = new Client({
-    intents:["Guilds"]
+    intents: ["Guilds", "GuildMessages", "GuildMessageReactions", "DirectMessages", "DirectMessageReactions"]
 });
 client.on("ready", () => {
     console.log(`Logged in as ${client.user?.tag}!`);
 });
 client.login(process.env.DISCORD_BOT_TOKEN).catch(console.error);
+
+export const ClientDo =
+    (callback: () => void): void => {
+
+        if (client.isReady()) {
+            callback();
+        } else {
+            client.on("ready", callback);
+        }
+
+    }
+
+export const GetChannelById = (id: string): Channel | undefined => {
+    return client.channels.cache.get(id);
+}
+
+export const AVATAR_URL = process.env.AVATAR_URL!;
+
+export const ADD_REACTION = "👍";
+export const REMOVE_REACTION = "👎";
+export const GetEmbed = (type: EmbedType): EmbedBuilder => {
+    switch (type) {
+        case "add":
+            return new EmbedBuilder()
+                .setColor("#00FFFF")
+                .setAuthor({
+                    iconURL: AVATAR_URL,
+                    name: "Hisu"
+                })
+        case "remove":
+            break;
+    }
+
+};
+
+export type EmbedType = "add" | "remove" | "subscribed";
 
 export default client;
--- pages/api/lhp.ts
+++ pages/api/lhp.ts
@@ -1,44 +1,59 @@
+"use server";
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {LHP} from "@/app/services/Storage";
+import client, {ClientDo, GetChannelById, GetEmbed} from "@/app/services/DiscordBot";
+import {Channel} from "node:diagnostics_channel";
+import {BaseChannel, EmbedBuilder} from "discord.js";
 
+client;
 type LHPData = {
     LHP: string | undefined | null;
     secret: string | undefined | null
 }
 
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<LHPData | LHPData[]>
 ) {
+
     if (req.method === "GET") {
         const query: string | string[] | undefined = req.query.LHP;
         if (query !== undefined)
             LHP.findOne({LHP: req.query.LHP as string}).then((lhp) => {
                 res.status(200).json({
-                    LHP: lhp?.LHP,
-                    secret: lhp?.secret
+                    LHP: lhp.LHP,
+                    secret: lhp.secret
                 });
             });
         else {
             LHP.find().then((lhps) => {
-                const list: LHPData[] = [];
-                for (const lhp of lhps) {
-                    list.push({
-                        LHP: lhp.LHP,
-                        secret: lhp.secret
-                    });
-                }
+                const list: LHPData[] = [...lhps];
                 res.status(200).json(list);
             });
         }
     } else if (req.method === "POST") {
         const body: LHPData = req.body;
-        res.status(200).end(); // Method not allowed
+        res.status(200);
         const newLHP = new LHP(body);
         newLHP.save().then(() => {
-            console.log("LHP saved");
+            res.status(201);
+            ClientDo(() => {
+                const channel = GetChannelById(process.env.NOTIFY_CHANNEL_ID!)
+                if (channel !== undefined) {
+                    const addEmbed = GetEmbed("add")
+                        .setTitle('Success: ' + body.LHP)
+                        .setDescription(body.secret!)
+                        .setTimestamp()
+                    if (channel.isTextBased()) {
+                        channel.send({embeds: [addEmbed]});
+                    }
+                }
+            })
+
         });
     } else {
-        res.status(200).end(); // Method not allowed
+        res.status(403);
     }
+    res.end();
 }
