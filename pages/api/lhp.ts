52cc0937857a414046575b1e4c27e9279e8c54d9
htilssu <tolashuu@gmail.com>
2024-02-28 02:30:47 +0700

    add get all lhp

--- pages/api/lhp.ts
+++ pages/api/lhp.ts
@@ -8,19 +8,32 @@ type LHPData = {
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<LHPData>
+    res: NextApiResponse<LHPData | LHPData[]>
 ) {
     if (req.method === "GET") {
-        LHP.findOne({LHP: req.query.LHP as string}).then((lhp) => {
-            console.log(lhp?.LHP, lhp?.secret)
-            res.status(200).json({
-                LHP: lhp?.LHP,
-                secret: lhp?.secret
+        const query: string | string[] | undefined = req.query.LHP;
+        if (query !== undefined)
+            LHP.findOne({LHP: req.query.LHP as string}).then((lhp) => {
+                res.status(200).json({
+                    LHP: lhp?.LHP,
+                    secret: lhp?.secret
+                });
             });
-        });
+        else {
+            LHP.find().then((lhps) => {
+                const list: LHPData[] = [];
+                for (const lhp of lhps) {
+                    list.push({
+                        LHP: lhp.LHP,
+                        secret: lhp.secret
+                    });
+                }
+                res.status(200).json(list);
+            });
+        }
     } else if (req.method === "POST") {
         const body: LHPData = req.body;
-        res.status(200).json(body);
+        res.status(200).end(); // Method not allowed
         const newLHP = new LHP(body);
         newLHP.save().then(() => {
             console.log("LHP saved");
@@ -28,5 +41,4 @@ export default async function handler(
     } else {
         res.status(200).end(); // Method not allowed
     }
-    res.status(200).end();
 }
